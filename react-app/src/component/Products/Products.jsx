c8ad6990ebd24c6e640257bc80df0bd0c3dc0ec0
Sebas <sebastianv.08.2000@gmail.com>
2022-10-19 17:48:33 -0300

    products

---
+++ react-app/src/component/Products/Products.jsx
@@ -0,0 +1,25 @@
+const products = [
+    {category:"strategy",image: "https://i.ebayimg.com/images/g/724AAOSwB-1YyeTu/s-l500.jpg",
+    price: 2000, stock: 10, title:"Catan"},
+
+    {category: "adventure",image: "https://target.scene7.com/is/image/Target/GUEST_e3a3850d-7137-4be4-86e0-31a5e60907c8?wid=325&hei=325&qlt=80&fmt=pjpeg",
+    price: 1200,stock:10, title:"Munchkin"},
+
+    {category :"strategy",image:"https://yetem.com/wp-content/uploads/2017/06/teg-clasico-570x570.jpg",price:3200,stock:10,title:"TEG"},
+
+    {category: "rol",image:"https://d3ugyf2ht6aenh.cloudfront.net/stores/907/020/products/ok_1024_dd-player-guide-5ed-juegos-de-mesa-abracadabra41-340b33a0c920c1e00a15857958611999-640-0.jpg",price:3000,stock:10,title:"D&D"},
+
+    
+    {category: "rol",image:"https://cdn.paizo.com/image/product/catalog/PZO/PZO1110.jpg",price:5000,stock:10,title:"Pathfinder"},
+
+    {category: "strategy",image:"https://http2.mlstatic.com/D_NQ_NP_852178-MLA44678979171_012021-O.webp",price:700,stock:10,title:"Monopoly"},
+
+    
+    {category: "adventure",image:"https://www.cazagangas.com.ar/images/juegos/webp/gloomhaven.webp",price:8000,stock:10,title:"Gloomhaven"},
+
+    {category: "adventure",image:"https://cdn.shopify.com/s/files/1/1915/6645/products/cthulhu-wars-core-game-fourth-printing-cw-o3-retail-pre-order-retail-board-game-petersen-games-31524065149080.jpg?v=1628654091",price:6500,stock:10,title:"Cthulu Wars"},
+
+];
+
+
+export default products;
